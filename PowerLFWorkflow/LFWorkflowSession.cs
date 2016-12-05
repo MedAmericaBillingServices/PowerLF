6e67e926a079a6973c7c431776d7d40900625ee4
Stefan Nuxoll <nuxolls@medamerica.com>
2016-12-05 15:13:27 -0700

    Added New-LFWorkflowProfile cmdlet and fixed JSON.net binding issue

--- PowerLFWorkflow/LFWorkflowSession.cs
+++ PowerLFWorkflow/LFWorkflowSession.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Laserfiche.Workflow;
 using Laserfiche.Workflow.Objects;
+using Laserfiche.Workflow.Objects.Options;
 
 namespace PowerLFWorkflow
 {
@@ -20,6 +22,17 @@ namespace PowerLFWorkflow
         internal IEnumerable<DataSource> GetAllDatasources()
         {
             return _workflowConnection.Settings.DataSources.GetAllItems();
+        }
+
+        internal void CreateProfile(string server, string repository, string username, string password, string profileName)
+        {
+            var profile = _workflowConnection.Settings.LaserficheProfiles.ConstructNewItem();
+            profile.ProfileName = profileName;
+            profile.ProfileServer = server;
+            profile.ProfileRepository = repository;
+            profile.ProfileUser = username;
+            profile.SetProfilePassword(password);
+            profile.Create();
         }
     }
 }
