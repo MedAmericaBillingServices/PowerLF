878b241aec91fc6ae6e4e3b9fbb39f44d7de504e
Overall, Matthew <overallm@medamerica.com>
2016-12-06 15:13:42 -0700

    Volume Info added, can create simple volumes

---
+++ PowerLFRepository/Data/LFVolumeInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerLFRepository.Data
+{
+	public class LFVolumeInfo
+	{
+		public string FixedPath { get; set; }
+		public int Id { get; internal set; }
+		public bool IsCompressed { get; set; }
+		public bool IsConnected { get; set; }
+		public bool IsDataChecksummed { get; internal set; }
+		public bool IsEDocFixed { get; internal set; }
+		public bool IsImageFixed { get; internal set; }
+		public bool IsLocked { get; internal set; }
+		public bool IsMounted { get; set; }
+		public bool IsOffline { get; internal set; }
+		public int SourceVolumeId { get; internal set; }
+		public bool IsPending { get; set; }
+		public DateTime RolloverBaseDate { get; set; }
+		public string Name { get; set; }
+		public int RolloverPeriod { get; set; }
+		public bool IsEncrypted { get; set; }
+		public bool IsModified { get; set; }
+		public bool IsNew { get; set; }
+		public bool IsReadOnly { get; set; }
+		public bool IsTextFixed { get; set; }
+		public string LocalId { get; set; }
+		public bool IsWriteOnce { get; set; }
+		public long MaximumSize { get; set; }
+		public string RemovablePath { get; set; }
+		public DateTime RolloverDate { get; set; }
+		public bool IsRollover { get; internal set; }
+	}
+}
--- PowerLFRepository/LFSession.cs
+++ PowerLFRepository/LFSession.cs
@@ -8,68 +8,68 @@ using PowerLFRepository.Data;
 
 namespace PowerLFRepository
 {
-    public class LFSession
+	public class LFSession
 	{
 		internal Session Session { get; set; }
 
-	    public string ServerName => Session.Repository.ServerName;
+		public string ServerName => Session.Repository.ServerName;
 
-	    public string RepositoryName => Session.Repository.Name;
+		public string RepositoryName => Session.Repository.Name;
 
-	    internal LFSession(string serverName, string repositoryName, string userName = null, string password = null)
-	    {
-	        Session = userName == null ? Session.Create(serverName, repositoryName) : Session.Create(serverName, repositoryName, userName, password);
-	        Session.ApplicationName = "PowerLF";
-	    }
+		internal LFSession(string serverName, string repositoryName, string userName = null, string password = null)
+		{
+			Session = userName == null ? Session.Create(serverName, repositoryName) : Session.Create(serverName, repositoryName, userName, password);
+			Session.ApplicationName = "PowerLF";
+		}
 
-        /// <summary>
-        /// Creates an account in the Laserfiche Repository
-        /// </summary>
-        /// <param name="sid">SecurityIdentifier for AD Trustee to add</param>
-        /// <param name="featureRightsList"></param>
-        /// <returns></returns>
-        internal LFTrustee AddTrustee(SecurityIdentifier sid, List<string> featureRights)
-        {
-            FeatureRights rights;
+		/// <summary>
+		/// Creates an account in the Laserfiche Repository
+		/// </summary>
+		/// <param name="sid">SecurityIdentifier for AD Trustee to add</param>
+		/// <param name="featureRightsList"></param>
+		/// <returns></returns>
+		internal LFTrustee AddTrustee(SecurityIdentifier sid, List<string> featureRights)
+		{
+			FeatureRights rights;
 
-            if (Enum.TryParse(string.Join(", ", featureRights), out rights) == false)
-            {
-                throw new ArgumentException("Failed to parse Feature Rights", nameof(featureRights));
-            }
+			if (Enum.TryParse(string.Join(", ", featureRights), out rights) == false)
+			{
+				throw new ArgumentException("Failed to parse Feature Rights", nameof(featureRights));
+			}
 
-            var trustee = new TrusteeInfo();
-            trustee.FeatureRights = rights;
+			var trustee = new TrusteeInfo();
+			trustee.FeatureRights = rights;
 
-            Trustee.SetInfo(new AccountReference(sid, Session), trustee, Session);
-            Session.Save();
+			Trustee.SetInfo(new AccountReference(sid, Session), trustee, Session);
+			Session.Save();
 
-            return new LFTrustee(Trustee.GetInfo(sid, Session));
-        }
+			return new LFTrustee(Trustee.GetInfo(sid, Session));
+		}
 
 
-        /// <summary>
-        /// Removes an account from the Laserfiche Repository
-        /// </summary>
-        /// <param name="identity">Active Directory principal to remove</param>
-        internal void RemoveAccount(Principal identity)
-        {
-            Trustee.DeleteSecurity(identity.Sid, Session);
+		/// <summary>
+		/// Removes an account from the Laserfiche Repository
+		/// </summary>
+		/// <param name="identity">Active Directory principal to remove</param>
+		internal void RemoveAccount(Principal identity)
+		{
+			Trustee.DeleteSecurity(identity.Sid, Session);
 
-            Session.Save();
-        }
+			Session.Save();
+		}
 
-#region Trustee management
+		#region Trustee management
 
-        internal void RemoveTrustee(SecurityIdentifier sid)
-        {
-            var info = Trustee.GetInfo(sid, Session);
+		internal void RemoveTrustee(SecurityIdentifier sid)
+		{
+			var info = Trustee.GetInfo(sid, Session);
 
-            info.Delete();
+			info.Delete();
 
-            info.Save();
+			info.Save();
 
-            Session.Save();
-        }
+			Session.Save();
+		}
 
 		internal void DenyTrustee(Principal identity)
 		{
@@ -85,17 +85,17 @@ namespace PowerLFRepository
 			trustee.FeatureRights = featureRights;
 			Session.Save();
 
-		    return Account.GetInfo(identity.Sid, Session);
+			return Account.GetInfo(identity.Sid, Session);
 		}
 
-        internal List<LFTrustee> GetAllTrustees()
-        {
-            return Trustee.EnumAllWindowsAccounts(Session).AsEnumerable().Select(x => new LFTrustee(x)).ToList();
-        }
+		internal List<LFTrustee> GetAllTrustees()
+		{
+			return Trustee.EnumAllWindowsAccounts(Session).AsEnumerable().Select(x => new LFTrustee(x)).ToList();
+		}
 
-#endregion
+		#endregion
 
-        internal void SetUserAccess(string username, FeatureRights featureRights, Privileges privileges)
+		internal void SetUserAccess(string username, FeatureRights featureRights, Privileges privileges)
 		{
 			var trustee = Account.GetInfo(username, AccountInfo.AccountFields.All, Session);
 			trustee.Privileges = privileges;
@@ -117,30 +117,44 @@ namespace PowerLFRepository
 			return Account.Create(user, false, Session);
 		}
 
-	    internal void RemoveUser(AccountInfo userInfo)
-	    {
-	        userInfo.Delete();
-	        Session.Save();
-	    }
+		internal void RemoveUser(AccountInfo userInfo)
+		{
+			userInfo.Delete();
+			Session.Save();
+		}
 
-	    internal AccountInfo GetUser(string username)
-	    {
-	            return Account.GetInfo(username, AccountInfo.AccountFields.All, Session);
-	    }
+		internal AccountInfo GetUser(string username)
+		{
+			return Account.GetInfo(username, AccountInfo.AccountFields.All, Session);
+		}
 
-	    internal AccountInfo GetUser(Principal principal)
-	    {
-	        return Account.GetInfo(principal.Sid, AccountInfo.AccountFields.All, Session);
-	    }
+		internal AccountInfo GetUser(Principal principal)
+		{
+			return Account.GetInfo(principal.Sid, AccountInfo.AccountFields.All, Session);
+		}
 
-	    internal List<AccountInfo> GetAllUsers()
-	    {
-	        return Account.EnumUsers(Session).AsEnumerable().ToList();
-	    }
+		internal List<AccountInfo> GetAllUsers()
+		{
+			return Account.EnumUsers(Session).AsEnumerable().ToList();
+		}
 
-        public LFTrustee GetTrustee(SecurityIdentifier sid)
-        {
-            return new LFTrustee(Trustee.GetInfo(sid, Session));
-        }
+		public LFTrustee GetTrustee(SecurityIdentifier sid)
+		{
+			return new LFTrustee(Trustee.GetInfo(sid, Session));
+		}
+
+		public int CreateVolume(string name, string fixedPath, bool rollover, long maxSizeMb)
+		{
+			var info = Volume.Create(new VolumeInfo
+			{
+				VolumeType = rollover ? VolumeType.Logical : VolumeType.Physical,
+				FixedPath = fixedPath,
+				Name = name,
+				MaximumSize = maxSizeMb,
+
+			}, Session);
+
+			return info.Id;
+		}
 	}
 }
